87f9d25ad7093e8b31694c2e93acde74b3329008
Njord <username@users.noreply.github.com>
2020-02-05 15:21:12 +0100

    minor bug fixes

--- wifi-scan.py
+++ wifi-scan.py
@@ -25,55 +25,55 @@ parser.add_argument("-i","--interface", help="Network interface where packets wi
 args = parser.parse_args()
 
 def main(interface):
-	conf.iface = interface
+    conf.iface = interface
 
-	rand_channel_t = Thread(target=rand_channel, args=(interface,))
-	rand_channel_t.start()
+    rand_channel_t = Thread(target=rand_channel, args=(interface,))
+    rand_channel_t.start()
 
-	print("Channel \t BSSID \t\t\t SSID")
-	print("------- \t ----- \t\t\t ----")
+    print("Channel \t BSSID \t\t\t SSID")
+    print("------- \t ----- \t\t\t ----")
 
 
-	try:
-		sniff(prn=packet_filter)
-	except KeyboardInterrupt:
-		sys.exit("[!] CTRL+C, exiting...")
+    try:
+        sniff(prn=packet_filter)
+    except KeyboardInterrupt:
+        sys.exit("[!] CTRL+C, exiting...")
 
 def rand_channel(interface):
-	while 1:
-		rand_channel = random.randint(1, 14)
+    while 1:
+        rand_channel = random.randint(1, 14)
 
-		subprocess.Popen("iwconfig {} channel {}".format(interface, int(rand_channel)), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-		time.sleep(0.1)
+        subprocess.Popen("iwconfig {} channel {}".format(interface, int(rand_channel)), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(0.1)
 
 def packet_filter(packet):
-	if packet.haslayer(Dot11Beacon) or packet.haslayer(Dot11ProbeResp):
-		if not packet.addr3 in AP_LIST:
-			AP_LIST.append(packet.addr3)
-			ssid = packet[Dot11Elt].info if not b"\x00" in packet[Dot11Elt].info and packet[Dot11Elt].info != "" else "Unknow ssid"
-			bssid = packet.addr3
-			channel = int(ord(packet[Dot11Elt:3].info))
+    if packet.haslayer(Dot11Beacon) or packet.haslayer(Dot11ProbeResp):
+        if not packet.addr3 in AP_LIST:
+            AP_LIST.append(packet.addr3)
+            ssid = packet[Dot11Elt].info if not b"\x00" in packet[Dot11Elt].info and packet[Dot11Elt].info != "" else "Unknow ssid"
+            bssid = packet.addr3
+            channel = int(ord(packet[Dot11Elt:3].info))
 
-			print("{} \t\t {} \t {}".format(channel, bssid, ssid.decode()))
+            print("{} \t\t {} \t {}".format(channel, bssid, ssid.decode()))
 
 
 if __name__ == "__main__":
-	AP_LIST = []
-	DEV_LIST = []
+    AP_LIST = []
+    DEV_LIST = []
 
-	if sys.platform.startswith('linux'):
-		if os.getuid() != 0:
-			sys.exit("[!] This script need to run as root to work properly")
-
-
-	elif sys.platform.startswith("win"):
-		sys.exit("[!] This script is for linux only!")
+    if sys.platform.startswith('linux'):
+        if os.getuid() != 0:
+            sys.exit("[!] This script need to run as root to work properly")
 
 
-	try:
-		print("[+] Importing scapy...", end=" ")
-		from scapy.all import *
-		print("Done.")
-	except ImportError:
-		sys.exit("\n[!] Error happened while importing scapy")
-	main(args.interface)
+    elif sys.platform.startswith("win"):
+        sys.exit("[!] This script is for linux only!")
+
+
+    try:
+        print("[+] Importing scapy...", end=" ")
+        from scapy.all import *
+        print("Done.")  
+    except ImportError:
+        sys.exit("\n[!] Error happened while importing scapy")
+    main(args.interface)
